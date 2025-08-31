bc2a902335c2249310a26f966a3967df88a38520
7899VRC <138609260+7899VRC@users.noreply.github.com>
2025-08-31 15:05:08 -0700

    added drivepid

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,13 @@ motor_group leftSide = motor_group(LF, LB, LM);
 motor_group rightSide = motor_group(RF, RB, RM);
 bool topPistonExtended = false;
 bool sidePistonExtended = false;
+double leftEncoders = leftSide.position(degrees);
+double rightEncoders = rightSide.position(degrees);
+double currentPosition = (leftEncoders + rightEncoders) / 2;
+// pid constants
+double pi = 3.1415926;
+double diameter = 3.25;
+double g = 36/48; // gear ratio 
 
 void drive(int lspeed, int rspeed, int wt) {
   leftSide.spin(forward, lspeed, pct);
@@ -51,15 +58,31 @@ void pistonControlSide() {
   sidePiston.set(sidePistonExtended);
 }
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+void inchDrive(float target) {
+  float kp = 2;
+  float kd = 0;
+  float error;
+  leftSide.resetPosition();
+  rightSide.resetPosition();
+  float x = 0;
+  float accuracy = 0.5;
+  float prevError = 0;
+  float derivative;
+  float output;
+  int time = 10;
+  while (error > accuracy) {
+    x = (currentPosition * pi * diameter) / 360;
+    error = (target - x);
+    derivative = (error - prevError) / time;
+    output = kp * error + (kd * derivative);
+    leftSide.spin(forward, error, pct);
+    rightSide.spin(forward, error, pct);
+    prevError = error; 
+    wait(time, msec);
+  }
+  leftSide.stop();
+  rightSide.stop();
+}
 
 void pre_auton(void) {
 
@@ -67,31 +90,11 @@ void pre_auton(void) {
   // Example: clearing encoders, setting servo positions, ...
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  leftSide.setStopping(brake);
+  rightSide.setStopping(brake);
+  inchDrive(10);
 }
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
   // User control code here, inside the loop
